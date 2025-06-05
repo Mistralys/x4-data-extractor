041c4ae01809a93201ececd5e6cb2f15edcec8c1
Mistralys <s.mordziol@gmail.com>
2025-06-05 14:13:19 +0200

    Config file.

---
+++ dev-config.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\X4;
+
+/**
+ * Absolute path to the X4 game folder.
+ */
+const X4_GAME_FOLDER = '/path/to/x4';
+
+/**
+ * Absolute path to the X Catalog Tool binary.
+ */
+const X4_CATALOG_TOOL_BINARY = __DIR__.'/XRCatTool.exe';
+
+/**
+ * Absolute path to the folder where the game files
+ * should be extracted to.
+ */
+const X4_EXTRACTED_CAT_FILES_FOLDER = '/path/to/folder';
