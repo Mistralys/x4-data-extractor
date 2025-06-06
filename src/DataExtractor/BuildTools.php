af9286ea0fad6c1c1de03cfdec1bd07cb86ac6f3
Mistralys <s.mordziol@gmail.com>
2025-06-06 11:24:32 +0200

    Added the `list-folders` Composer command.

--- src/DataExtractor/BuildTools.php
+++ src/DataExtractor/BuildTools.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Mistralys\X4\DataExtractor;
 
+use AppUtils\FileHelper;
 use AppUtils\FileHelper\FileInfo;
 use AppUtils\FileHelper\FolderInfo;
+use AppUtils\FileHelper\JSONFile;
+use Mistralys\X4\ExtractedData\DataFolders;
+use Mistralys\X4\UI\Console;
 use const Mistralys\X4\X4_CATALOG_TOOL_BINARY;
 use const Mistralys\X4\X4_EXTRACTED_CAT_FILES_FOLDER;
 use const Mistralys\X4\X4_GAME_FOLDER;
@@ -15,7 +19,7 @@ class BuildTools
     private const BATCH_TEMPLATE = <<<'BATCH'
 @echo off
 
-%s %s
+%s
 
 pause
 
@@ -25,35 +29,86 @@ BATCH;
     {
         self::init();
 
-        $arguments = array();
+        $all = array();
+        foreach(self::getCatFiles() as $source => $def)
+        {
+            $arguments = array();
 
-        foreach(self::getCatFiles() as $catFile)  {
-            $arguments[] = '-in "'.$catFile->getPath().'"';
+            foreach($def['files'] as $catFile)  {
+                $arguments[] = '-in "'.$catFile->getPath().'"';
+            }
+
+            $outputFolder = X4_EXTRACTED_CAT_FILES_FOLDER.'/'.$source;
+            FileHelper::createFolder($outputFolder);
+
+            JSONFile::factory($outputFolder.'/info.json')
+                ->setPrettyPrint(true)
+                ->setTrailingNewline(true)
+                ->putData(array(
+                    'id' => $source,
+                    'label' => $def['label'],
+                    'isExtension' => $source !== CatFileFinder::SOURCE_VANILLA,
+                ));
+
+            $arguments[] = '-out "'.$outputFolder.'"';
+            $command = X4_CATALOG_TOOL_BINARY.' '.implode(' ', $arguments);
+
+            $all[] = $command;
+
+            $outputFile = FileInfo::factory(__DIR__.'/../../batch/unpack-'.$source.'.bat');
+            $outputFile->delete();
+
+            Console::line1('Saving to batch file [%s].', $outputFile->getName());
+
+            FileInfo::factory($outputFile)
+                ->putContents(sprintf(
+                    self::BATCH_TEMPLATE,
+                    $command
+                ));
         }
 
-        $arguments[] = '-out "'.X4_EXTRACTED_CAT_FILES_FOLDER.'"';
-
-        $outputFile = FileInfo::factory(__DIR__.'/../../unpack.bat');
-
+        $outputFile = FileInfo::factory(__DIR__.'/../../batch/unpack-all.bat');
         $outputFile->delete();
-
-        echo '- Saving to batch file ['.$outputFile->getName().'].'.PHP_EOL;
 
         FileInfo::factory($outputFile)
             ->putContents(sprintf(
                 self::BATCH_TEMPLATE,
-                X4_CATALOG_TOOL_BINARY,
-                implode(' ', $arguments)
+                implode(PHP_EOL, $all)
             ));
     }
 
     /**
-     * @return FileInfo[]
+     * @return array<string,array{files:FileInfo[], label:string}>
      */
     private static function getCatFiles() : array
     {
-        return (new CatFileFinder(FolderInfo::factory(X4_GAME_FOLDER)))
-            ->findFiles();
+        $result = array();
+
+        foreach((new CatFileFinder(FolderInfo::factory(X4_GAME_FOLDER)))->findFiles() as $file) {
+            $source = $file->getRuntimeProperty('source');
+            if(!isset($result[$source])) {
+                $result[$source] = array(
+                    'label' => $file->getRuntimeProperty('label'),
+                    'files' => array(),
+                );
+            }
+
+            $result[$source]['files'][] = $file;
+        }
+
+        return $result;
+    }
+
+    public static function listDataFolders() : void
+    {
+        self::init();
+
+        Console::header('Available Data Folders');
+
+        $folders = DataFolders::create(FolderInfo::factory(X4_EXTRACTED_CAT_FILES_FOLDER));
+        foreach($folders->getAll() as $folder) {
+            Console::line1($folder->getID().' - '.$folder->getLabel());
+        }
     }
 
     private static function init() : void
