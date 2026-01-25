69839eac9621aae5b164a8f149723804e1923c84
Mistralys <s.mordziol@gmail.com>
2026-01-25 16:55:29 +0100

    Added tests structure.

---
+++ tests/classes/BaseTestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\X4\Tests\Classes;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTestCase extends TestCase
+{
+
+}
---
+++ tests/unit-tests/ExtractedData/GameInfoTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\X4\Tests\UnitTests\ExtractedData;
+
+use Mistralys\X4\Tests\Classes\BaseTestCase;
+
+class GameInfoTest extends BaseTestCase
+{
+}
